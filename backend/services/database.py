9068b536e0f3d47988479dd70c2a657965570fe3
Sahith Akula <akulasahith268@gmail.com>
2026-05-24 01:37:37 +0530

    Database url updated

--- backend/services/database.py
+++ backend/services/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/startup_os"
+DATABASE_URL = "postgresql://neondb_owner:npg_9NGcDWur6qtd@ep-hidden-waterfall-aqwep9rq.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
